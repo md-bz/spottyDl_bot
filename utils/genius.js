e22515f663601fc173308e3b6f9bdcf8eeb4ee9b
mehrad <mmfirst@protonmail.com>
2024-06-23 19:35:50 +0330

    genius

--- utils/genius.js
+++ utils/genius.js
@@ -2,18 +2,26 @@ const Genius = require("genius-lyrics");
 const Client = new Genius.Client(process.env.GENIUS_TOkEN);
 
 async function getGenius(searchQuery) {
-    const searches = await Client.songs.search(searchQuery);
+    try {
+        const searches = await Client.songs.search(searchQuery);
 
-    if (!searches || searches.length === 0) {
-        return { track: "", lyrics: "" };
+        if (!searches || searches.length === 0) {
+            return { track: {}, lyrics: "" };
+        }
+        const track = searches[0];
+        const lyrics = await track.lyrics();
+
+        return {
+            track,
+            lyrics,
+        };
+    } catch (error) {
+        console.error(error);
+        return {
+            track: {},
+            lyrics: "",
+        };
     }
-    const track = searches[0];
-    const lyrics = await track.lyrics();
-
-    return {
-        track,
-        lyrics,
-    };
 }
 
 module.exports = getGenius;
